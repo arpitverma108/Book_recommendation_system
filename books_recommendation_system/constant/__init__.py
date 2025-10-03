be5af7cc33e9a2fc9ff029445543aa5c968a20ad
Arpit Verma <arpitv0710@gmail.com>
2025-10-04 02:55:59 +0530

    constant file updated

--- books_recommendation_system/constant/__init__.py
+++ books_recommendation_system/constant/__init__.py
@@ -5,10 +5,3 @@ ROOT_DIR = os.getcwd()
 CONFIG_FOLDER_NAME = "config"
 CONFIG_FILE_NAME = "config.yaml"
 CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_FOLDER_NAME,CONFIG_FILE_NAME)
-print("CONFIG_FILE_PATH:", CONFIG_FILE_PATH)  # 👈 Full path to config.yaml
-
-# Optional: Check if the file actually exists
-if os.path.exists(CONFIG_FILE_PATH):
-    print("✅ Config file exists!")
-else:
-    print("❌ Config file NOT found.")
