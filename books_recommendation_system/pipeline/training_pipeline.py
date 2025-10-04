36c5fc1c3a75178b2050c4f79aca384ef6cad563
Arpit Verma <arpitv0710@gmail.com>
2025-10-04 06:19:15 +0530

    data validation

--- books_recommendation_system/pipeline/training_pipeline.py
+++ books_recommendation_system/pipeline/training_pipeline.py
@@ -1,7 +1,10 @@
 from books_recommendation_system.components.stage_00_data_ingestion import DataIngestion
+from books_recommendation_system.components.stage_01_data_validation import DataValidation
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
 
     def start_data_ingestion(self):
         self.data_ingestion.initiate_data_ingestion()
+        self.data_validation.initiate_data_validation()
