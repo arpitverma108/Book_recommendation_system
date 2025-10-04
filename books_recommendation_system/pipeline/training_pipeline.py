beba121cbdda09b95120d52a646852669cfdc322
Arpit Verma <arpitv0710@gmail.com>
2025-10-04 15:17:18 +0530

    transformed data updated

--- books_recommendation_system/pipeline/training_pipeline.py
+++ books_recommendation_system/pipeline/training_pipeline.py
@@ -1,10 +1,14 @@
 from books_recommendation_system.components.stage_00_data_ingestion import DataIngestion
 from books_recommendation_system.components.stage_01_data_validation import DataValidation
+from books_recommendation_system.components.stage_02_data_transformation import DataTransformation
+
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
 
     def start_data_ingestion(self):
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
+        self.data_transformation.initiate_data_transformation()
